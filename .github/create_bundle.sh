96811265733eccdfd02f7fb351ddcdad7d59ea21
Amol Kahat <amolkahat@gmail.com>
2023-02-16 12:45:44 +0530

    Update pre-commit hook with bashate
    This patch removes golint and by default
    use golangci-lint.
    
    Signed-off-by: Amol Kahat <amolkahat@gmail.com>

--- .github/create_bundle.sh
+++ .github/create_bundle.sh
@@ -34,26 +34,26 @@ cat "${CLUSTER_BUNDLE_FILE}" | grep "image:"
 # error will be reported to the console.
 set +e
 for csv_image in $(cat "${CLUSTER_BUNDLE_FILE}" | grep "image:" | sed -e "s|.*image:||" | sort -u); do
-  digest_image=""
-  echo "CSV line: ${csv_image}"
+    digest_image=""
+    echo "CSV line: ${csv_image}"
 
-  # case where @ is in the csv_image image
-  if [[ "$csv_image" =~ .*"@".* ]]; then
-    delimeter='@'
-  else
-    delimeter=':'
-  fi
+    # case where @ is in the csv_image image
+    if [[ "$csv_image" =~ .*"@".* ]]; then
+        delimeter='@'
+    else
+        delimeter=':'
+    fi
 
-  base_image=$(echo $csv_image | cut -f 1 -d${delimeter})
-  tag_image=$(echo $csv_image | cut -f 2 -d${delimeter})
-  digest_image=$(skopeo inspect docker://${base_image}${delimeter}${tag_image} | jq '.Digest' -r)
-  echo "Base image: $base_image"
-  if [ -n "$digest_image" ]; then
-    echo "$base_image${delimeter}$tag_image becomes $base_image@$digest_image"
-    sed -i "s|$base_image$delimeter$tag_image|$base_image@$digest_image|g" "${CLUSTER_BUNDLE_FILE}"
-  else
-    echo "$base_image${delimeter}$tag_image not changed"
-  fi
+    base_image=$(echo $csv_image | cut -f 1 -d${delimeter})
+    tag_image=$(echo $csv_image | cut -f 2 -d${delimeter})
+    digest_image=$(skopeo inspect docker://${base_image}${delimeter}${tag_image} | jq '.Digest' -r)
+    echo "Base image: $base_image"
+    if [ -n "$digest_image" ]; then
+        echo "$base_image${delimeter}$tag_image becomes $base_image@$digest_image"
+        sed -i "s|$base_image$delimeter$tag_image|$base_image@$digest_image|g" "${CLUSTER_BUNDLE_FILE}"
+    else
+        echo "$base_image${delimeter}$tag_image not changed"
+    fi
 done
 
 echo "Resulting bundle file images:"
