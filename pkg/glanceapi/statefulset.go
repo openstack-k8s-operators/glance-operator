d8636775a73da79d0187afad1d33f18e9477146b
Sean Mooney <work@seanmooney.info>
2024-04-24 19:42:22 +0100

    Bump glance-api probe timeout
    when deploying locally in crc glance can crashloop
    indefintly because the readiness and liveness probe
    were set to strictly.
    
    The nova-operator relaxed the probes becuase of tempest failures
    in https://github.com/openstack-k8s-operators/nova-operator/pull/381
    
    This change updates the glance values to match.

--- pkg/glanceapi/statefulset.go
+++ pkg/glanceapi/statefulset.go
@@ -27,13 +27,14 @@ import (
 	"github.com/openstack-k8s-operators/lib-common/modules/common/tls"
 	"github.com/openstack-k8s-operators/lib-common/modules/storage"
 
+	"sort"
+
 	"golang.org/x/exp/maps"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/utils/ptr"
-	"sort"
 )
 
 const (
@@ -60,12 +61,13 @@ func StatefulSet(
 		PeriodSeconds:    10,
 	}
 	livenessProbe := &corev1.Probe{
-		PeriodSeconds:       3,
-		InitialDelaySeconds: 3,
+		TimeoutSeconds:      30,
+		PeriodSeconds:       30,
+		InitialDelaySeconds: 5,
 	}
 	readinessProbe := &corev1.Probe{
-		TimeoutSeconds:      5,
-		PeriodSeconds:       5,
+		TimeoutSeconds:      30,
+		PeriodSeconds:       30,
 		InitialDelaySeconds: 5,
 	}
 
