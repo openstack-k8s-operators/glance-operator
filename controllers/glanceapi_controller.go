6e2b244f171673d61cdc14a4abc65b442ebc3e6e
Francesco Pantano <fpantano@redhat.com>
2025-02-14 16:46:43 +0100

    Remove common topology logic provided by infra-operator
    This patch updates the topology related code to rely on the common
    functions provided by infra-operator.
    
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

--- controllers/glanceapi_controller.go
+++ controllers/glanceapi_controller.go
@@ -860,15 +860,19 @@ func (r *GlanceAPIReconciler) reconcileNormal(
 		Name:      instance.Status.LastAppliedTopology,
 		Namespace: instance.Namespace,
 	}
-	defaultAPISelector := fmt.Sprintf("%s-%s-%s", glance.ServiceName, instance.APIName(), instance.Spec.APIType)
 
-	topology, err := r.ensureGlanceAPITopology(
+	// Retrieve the referenced Topology
+	defaultLabelSelector := labels.GetSingleLabelSelector(
+		glance.GlanceAPIName,
+		fmt.Sprintf("%s-%s-%s", glance.ServiceName, instance.APIName(), instance.Spec.APIType),
+	)
+	topology, err := topologyv1.EnsureServiceTopology(
 		ctx,
 		helper,
 		instance.Spec.TopologyRef,
 		&lastTopologyRef,
 		instance.APIName(),
-		defaultAPISelector,
+		defaultLabelSelector,
 	)
 	if err != nil {
 		instance.Status.Conditions.Set(condition.FalseCondition(
@@ -1533,64 +1537,4 @@ func (r *GlanceAPIReconciler) glanceAPIRefresh(
 		return err
 	}
 	return nil
-}
-
-// ensureGlanceAPITopology - when a Topology CR is referenced, remove the
-// finalizer from a previous referenced Topology (if any), and retrieve the
-// newly referenced topology object
-func (r *GlanceAPIReconciler) ensureGlanceAPITopology(
-	ctx context.Context,
-	helper *helper.Helper,
-	tpRef *topologyv1.TopoRef,
-	lastAppliedTopology *topologyv1.TopoRef,
-	finalizer string,
-	selector string,
-) (*topologyv1.Topology, error) {
-
-	var podTopology *topologyv1.Topology
-	var err error
-
-	// Remove (if present) the finalizer from a previously referenced topology
-	//
-	// 1. a topology reference is removed (tpRef == nil) from the GlanceAPI
-	//    CR and the finalizer should be deleted from the last applied topology
-	//    (lastAppliedTopology != "")
-	// 2. a topology reference is updated in the GlanceAPI CR (tpRef != nil)
-	//    and the finalizer should be removed from the previously
-	//    referenced topology (tpRef.Name != lastAppliedTopology.Name)
-	if (tpRef == nil && lastAppliedTopology.Name != "") ||
-		(tpRef != nil && tpRef.Name != lastAppliedTopology.Name) {
-		_, err = topologyv1.EnsureDeletedTopologyRef(
-			ctx,
-			helper,
-			lastAppliedTopology,
-			finalizer,
-		)
-		if err != nil {
-			return nil, err
-		}
-	}
-	// TopologyRef is passed as input, get the Topology object
-	if tpRef != nil {
-		// no Namespace is provided, default to instance.Namespace
-		if tpRef.Namespace == "" {
-			tpRef.Namespace = helper.GetBeforeObject().GetNamespace()
-		}
-		// Retrieve the referenced Topology
-		defaultLabelSelector := labels.GetSingleLabelSelector(
-			glance.GlanceAPIName,
-			selector,
-		)
-		podTopology, _, err = topologyv1.EnsureTopologyRef(
-			ctx,
-			helper,
-			tpRef,
-			finalizer,
-			&defaultLabelSelector,
-		)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return podTopology, nil
 }
